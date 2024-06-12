babdbcd0b3ba222117e5c2ec771bef8c48827b14
karishma1827 <karishmasachin65@gmail.com>
2024-06-12 08:00:31 +0530

    reupload

--- func.py
+++ func.py
@@ -16,3 +16,20 @@ def registeration(username,age,address,course,duration):
     data["students"].append(temp_data)
     write_json(data)
     print()
+    
+def remove(id):
+    data=read_json()
+    name=""
+    for stud in data["students"]:
+        if stud["sno"]==int(id):
+            name=stud["username"]
+            data["students"].remove(stud)
+    
+    i=1
+    for stud in data["students"]:
+        stud["sno"]=i
+        i+=1
+    write_json(data)
+    return name
+            
+            
--- main.py
+++ main.py
@@ -4,12 +4,20 @@ from func import*
 app=Flask(__name__)
 
 @app.route("/",methods=["POST","GET"])
-
 def hello():
+    msg=""
     if request.method=="POST":
         registeration(username=request.form['username'],age=request.form['age'],
                      address=request.form['address'],course=request.form['course'],
                      duration=request.form['duration'])
+        msg=request.form['username']
+    data=read_json()
+    
+    return render_template("index.html",stud=data["students"],msg=msg)
+
+@app.route("/delete/<id>",methods=["POST","GET"])
+def delete(id):
+    remove(id)
     data=read_json()
     return render_template("index.html",stud=data["students"])
 
